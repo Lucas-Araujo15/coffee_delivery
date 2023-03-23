fecb4203e48f6ca5ff7af632ad97087a1768b171
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-03-23 18:51:27 -0300

    feat: adiciona header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,21 @@
+import { CartButton, HeaderContainer, Location } from './styles'
+import logo from '../../assets/logo-coffee-delivery.svg'
+import { MapPin, ShoppingCart } from '@phosphor-icons/react'
+
 export function Header() {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <HeaderContainer>
+      <img src={logo} alt="" />
+      <div>
+        <Location>
+          <MapPin weight="fill" size={22} />
+          <p>Porto Alegre, RS</p>
+        </Location>
+        <CartButton>
+          <ShoppingCart weight="fill" size={22} />
+          <span>3</span>
+        </CartButton>
+      </div>
+    </HeaderContainer>
   )
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  height: 6.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  img {
+    height: 2.5rem;
+  }
+
+  > div {
+    display: flex;
+    gap: 0.75rem;
+  }
+`
+
+export const Location = styled.div`
+  width: 8.9375rem;
+  height: 2.375rem;
+  background: ${(props) => props.theme['purple-light']};
+  border-radius: 6px;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  p {
+    font-size: 14px;
+    line-height: 18.2;
+    color: ${(props) => props.theme['purple-dark']};
+  }
+
+  svg {
+    color: ${(props) => props.theme.purple};
+  }
+`
+
+export const CartButton = styled.button`
+  width: 2.375rem;
+  height: 2.375rem;
+  background: ${(props) => props.theme['yellow-light']};
+  border: none;
+  border-radius: 6px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  svg {
+    color: ${(props) => props.theme['yellow-dark']};
+  }
+
+  span {
+    background: ${(props) => props.theme['yellow-dark']};
+    min-width: 1.25rem;
+    min-height: 1.25rem;
+    border-radius: 50%;
+    color: ${(props) => props.theme.white};
+    font-size: 0.75rem;
+    font-weight: 700;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    position: absolute;
+    margin-top: -2.25rem;
+    margin-right: -2.25rem;
+  }
+`
