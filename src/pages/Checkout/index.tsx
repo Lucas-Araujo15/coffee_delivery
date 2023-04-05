980a1c9161ea7c36f709425cb0fcc7deec3bd28a
Lucas Araujo <lucasaraujo4188@gmail.com>
2023-04-04 23:38:02 -0300

    feat: adiciona campo para método de pagamento e cafés selecionados na página Checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,10 +1,21 @@
-import { MapPinLine } from '@phosphor-icons/react'
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from '@phosphor-icons/react'
 import {
   OrderInformationBox,
   CheckoutContainer,
   SelectedCoffeesBox,
   AddressForm,
   FormInput,
+  PaymentMethod,
+  CardTitle,
+  Checkbox,
+  PriceInformation,
+  ConfirmButton,
 } from './styles'
 
 export function Checkout() {
@@ -12,30 +23,86 @@ export function Checkout() {
     <CheckoutContainer>
       <OrderInformationBox>
         <h2>Complete seu pedido</h2>
-        <AddressForm>
-          <header>
-            <MapPinLine size={22} />
+        <div>
+          <AddressForm>
+            <CardTitle iconColor="yellow-dark">
+              <MapPinLine size={22} />
+              <div>
+                <h3>Endereço de Entrega</h3>
+                <p>Informe o endereço onde deseja receber seu pedido</p>
+              </div>
+            </CardTitle>
+            <form action="">
+              <FormInput placeholder="CEP" />
+              <FormInput placeholder="Rua" />
+              <FormInput placeholder="Número" />
+              <FormInput placeholder="Complemento" />
+              <FormInput placeholder="Bairro" />
+              <FormInput placeholder="Cidade" />
+              <FormInput placeholder="UF" />
+            </form>
+          </AddressForm>
+          <PaymentMethod>
+            <CardTitle iconColor="purple">
+              <CurrencyDollar size={22} />
+              <div>
+                <h3>Pagamento</h3>
+                <p>
+                  O pagamento é feito na entrega. Escolha a forma que deseja
+                  pagar
+                </p>
+              </div>
+            </CardTitle>
             <div>
-              <h3>Endereço de Entrega</h3>
-              <p>Informe o endereço onde deseja receber seu pedido</p>
+              <input type="checkbox" id="credit" />
+              <input type="checkbox" id="debit" />
+              <input type="checkbox" id="cash" />
+
+              <label htmlFor="credit">
+                <Checkbox id="check-credit">
+                  <CreditCard size={16} />
+                  <p>cartão de crédito</p>
+                </Checkbox>
+              </label>
+
+              <label htmlFor="debit">
+                <Checkbox id="check-debit">
+                  <Bank size={16} />
+                  <p>cartão de débito</p>
+                </Checkbox>
+              </label>
+
+              <label htmlFor="cash">
+                <Checkbox id="check-cash">
+                  <Money size={16} />
+                  <p>dinheiro</p>
+                </Checkbox>
+              </label>
             </div>
-          </header>
-          <form action="">
-            <FormInput placeholder="CEP" />
-
-            <FormInput placeholder="Rua" />
-
-            <FormInput placeholder="Número" />
-            <FormInput placeholder="Complemento" />
-
-            <FormInput placeholder="Bairro" />
-            <FormInput placeholder="Cidade" />
-            <FormInput placeholder="UF" />
-          </form>
-        </AddressForm>
+          </PaymentMethod>
+        </div>
       </OrderInformationBox>
       <SelectedCoffeesBox>
         <h2>Cafés selecionados</h2>
+        <div>
+          <PriceInformation>
+            <div>
+              <p>Total de itens</p>
+              <p>R$ 29,70</p>
+            </div>
+            <div>
+              <p>Entrega</p>
+              <p>R$ 3,50</p>
+            </div>
+            <div>
+              <p>Total</p>
+              <p>R$ 33,20</p>
+            </div>
+          </PriceInformation>
+          <ConfirmButton>
+            <p>confirmar pedido</p>
+          </ConfirmButton>
+        </div>
       </SelectedCoffeesBox>
     </CheckoutContainer>
   )
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -17,20 +17,47 @@ export const CheckoutContainer = styled.div`
 
 export const OrderInformationBox = styled.div`
   min-height: 39.3125rem;
-`
 
-export const SelectedCoffeesBox = styled.div`
-  height: 70px;
+  & > div {
+    display: grid;
+    height: 100%;
+    grid-template-rows: 23.25rem 1fr;
+    gap: 0.9375rem;
+  }
 `
 
 export const AddressForm = styled.div`
-  min-height: 23.25rem;
   border-radius: 6px;
   padding: 2.5rem;
   background: ${(props) => props.theme['base-card']};
   display: flex;
   flex-direction: column;
   gap: 2rem;
+
+  form {
+    display: grid;
+    grid-gap: 1rem 0.75rem;
+    grid-template-columns: 12.5rem 17.25rem 1fr;
+  }
+`
+
+const ICONS_COLORS = {
+  'yellow-dark': 'yellow-dark',
+  purple: 'purple',
+} as const
+
+interface IconsProps {
+  iconColor: keyof typeof ICONS_COLORS
+}
+
+export const CardTitle = styled.header<IconsProps>`
+  min-height: 2.75rem;
+  display: flex;
+  gap: 0.5rem;
+
+  svg {
+    color: ${(props) => props.theme[ICONS_COLORS[props.iconColor]]};
+  }
 
   h3 {
     font-weight: 400;
@@ -44,26 +71,10 @@ export const AddressForm = styled.div`
     color: ${(props) => props.theme['base-text']};
   }
 
-  header {
-    min-height: 2.75rem;
+  div {
     display: flex;
-    gap: 0.5rem;
-
-    svg {
-      color: ${(props) => props.theme['yellow-dark']};
-    }
-
-    div {
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-    }
-  }
-
-  form {
-    display: grid;
-    grid-gap: 1rem 0.75rem;
-    grid-template-columns: 12.5rem 17.25rem 1fr;
+    flex-direction: column;
+    justify-content: space-between;
   }
 `
 
@@ -82,5 +93,144 @@ export const FormInput = styled.input`
 
   &:nth-child(4) {
     grid-column: 2 / 4;
+  }
+`
+
+export const PaymentMethod = styled.div`
+  background: ${(props) => props.theme['base-card']};
+  padding: 2.5rem;
+  border-radius: 6px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
+  input {
+    display: none;
+  }
+
+  #credit:checked ~ label {
+    #check-credit {
+      transition: 0.1s;
+      border: 1px solid ${(props) => props.theme.purple};
+      background: ${(props) => props.theme['purple-light']};
+    }
+  }
+
+  #debit:checked ~ label {
+    #check-debit {
+      transition: 0.1s;
+      border: 1px solid ${(props) => props.theme.purple};
+      background: ${(props) => props.theme['purple-light']};
+    }
+  }
+
+  #cash:checked ~ label {
+    #check-cash {
+      transition: 0.1s;
+      border: 1px solid ${(props) => props.theme.purple};
+      background: ${(props) => props.theme['purple-light']};
+    }
+  }
+
+  > div {
+    display: flex;
+    justify-content: space-between;
+  }
+`
+
+export const Checkbox = styled.div`
+  width: 11.166875rem;
+  height: 3.1875rem;
+  background: ${(props) => props.theme['base-button']};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 6px;
+  gap: 0.75rem;
+  cursor: pointer;
+  transition: 0.1s;
+
+  &:hover {
+    background: ${(props) => props.theme['base-hover']};
+  }
+
+  p {
+    text-transform: uppercase;
+    font-weight: 400;
+    font-size: 0.75rem;
+    color: ${(props) => props.theme['base-text']};
+  }
+
+  svg {
+    color: ${(props) => props.theme.purple};
+  }
+`
+
+export const SelectedCoffeesBox = styled.div`
+  & > div {
+    background: ${(props) => props.theme['base-card']};
+    padding: 2.5rem;
+    border-radius: 6px 44px;
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+  }
+`
+
+export const PriceInformation = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  & div:nth-child(1),
+  & div:nth-child(2) {
+    p {
+      color: ${(props) => props.theme['base-text']};
+      font-weight: 400;
+    }
+
+    p:nth-child(1) {
+      font-size: 0.875rem;
+    }
+    p:nth-child(2) {
+      font-size: 1rem;
+    }
+  }
+
+  & div:nth-child(3) {
+    p {
+      font-weight: 700;
+      font-size: 1.25rem;
+      color: ${(props) => props.theme['base-subtitle']};
+    }
+  }
+`
+
+export const ConfirmButton = styled.button`
+  height: 2.875rem;
+  background: ${(props) => props.theme.yellow};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  border-radius: 6px;
+  transition: 0.2s;
+  cursor: pointer;
+
+  &:hover {
+    transition: 0.2s;
+    background: ${(props) => props.theme['yellow-dark']};
+  }
+
+  p {
+    text-transform: uppercase;
+    color: ${(props) => props.theme.white};
+    font-weight: 700;
+    font-size: 0.875rem;
   }
 `
